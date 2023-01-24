981b88c178e0d6670f3404cf57f1c37b643c53bf
JakariaJishan <jakariajishan987@gmail.com>
2023-01-25 02:15:24 +0600

    Implement list structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import _ from 'lodash';
-import './style.css';
-
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import "./style.css";
+let todos = [
+  {
+    desc: "todo 1",
+    completed: false,
+    index: 0,
+  },
+  {
+    desc: "todo 2",
+    completed: false,
+    index: 1,
+  },
+  {
+    desc: "todo 3",
+    completed: true,
+    index: 3,
+  },
+];
+let todoList = document.getElementById("todo-list");
+let ul = document.createElement("ul");
+todos.forEach((todo) => {
+    let li = document.createElement("li");
+    let list = '';
+  list += ` <div class='list'><input type="checkbox"><p>${todo.desc}</p></div> <div class="edit-icon"></div>
+  `;
+  li.innerHTML = list;
+  ul.appendChild(li)
+});
+todoList.appendChild(ul)
