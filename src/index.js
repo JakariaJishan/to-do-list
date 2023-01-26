b84bedb21800618ed0dcfb74e66d05453cc59ab2
JakariaJishan <jakariajishan987@gmail.com>
2023-01-26 17:32:57 +0600

    Add and refactor add and remove functinality

--- src/index.js
+++ src/index.js
@@ -1,70 +1,96 @@
-import "./style.css";
+import './style.css';
 
 let todoArr = [];
 
-todoArr = JSON.parse(localStorage.getItem("todos")) || [];
-let form = document.querySelector("form");
+todoArr = JSON.parse(localStorage.getItem('todos')) || [];
+const form = document.querySelector('form');
 
-let addTodo = (userTask) => {
+// add todo list
+
+const addTodo = (userTask) => {
   todoArr.push({
     id: todoArr.length + 1,
     desc: userTask,
     completed: false,
   });
-  localStorage.setItem("todos", JSON.stringify(todoArr) || []);
+  localStorage.setItem('todos', JSON.stringify(todoArr) || []);
 };
 
-let updateTask = (value,id) =>{
-  let todos = JSON.parse(localStorage.getItem('todos'));
-  let newArr = todos.findIndex(elem => elem.id == id)
-  todos[newArr].desc= value
-  localStorage.setItem("todos", JSON.stringify(todos) || []);
+// update todo
 
-}
+const updateTask = (value, id) => {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  const newArr = todos.findIndex((elem) => elem.id === id);
+  todos[newArr].desc = value;
+  localStorage.setItem('todos', JSON.stringify(todos) || []);
+};
 
-form.addEventListener("submit", (e) => {
+// remove item
+
+const removeTodo = (id) => {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  const newTodos = todos.filter((item) => item.id !== id);
+  // newTodos.map((elem, index) => newTodos[index].id = index + 1);
+  for (let i = 0; i < newTodos.length; i += 1) {
+    newTodos[i].id = i + 1;
+  }
+  localStorage.setItem('todos', JSON.stringify(newTodos) || []);
+  window.location.reload();
+};
+
+// get user input value
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  let userTask = document.getElementById("user-task").value;
+  const userTask = document.getElementById('user-task').value;
   addTodo(userTask);
-  document.getElementById("user-task").value = "";
-  displayTodo();
+  document.getElementById('user-task').value = '';
+  window.location.reload();
 });
 
-let displayTodo = () => {
-  let todos = JSON.parse(localStorage.getItem("todos"));
-  let todoList = document.getElementById("todo-list");
-  let ul = document.createElement('ul')
-  ul.setAttribute('class', 'ul-list')
+// display todo
+
+const displayTodo = () => {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  const todoList = document.getElementById('todo-list');
+  const ul = document.createElement('ul');
+  ul.setAttribute('class', 'ul-list');
   todos.forEach((todo) => {
-    let li = document.createElement('li');
-    li.setAttribute('class', 'list')
-    li.setAttribute('id', todo.id)
-    li.innerHTML += todo.desc
-    ul.appendChild(li)
+    const li = document.createElement('li');
+    const checkBox = document.createElement('input');
+    const p = document.createElement('p');
+    const removeBtn = document.createElement('button');
+    removeBtn.setAttribute('id', 'removeItem');
+    removeBtn.setAttribute('class', 'delete-icon');
+    checkBox.type = 'checkbox';
+    p.setAttribute('class', 'list');
+    p.setAttribute('id', todo.id);
+    li.appendChild(checkBox);
+    li.appendChild(p);
+    li.appendChild(removeBtn);
+    p.innerHTML += todo.desc;
+    ul.appendChild(li);
+    removeBtn.addEventListener('click', () => {
+      removeTodo(todo.id);
+    });
   });
-  todoList.appendChild(ul) ;
+  todoList.appendChild(ul);
 };
 
 displayTodo();
 
+// make item editable
 
-document.querySelectorAll('.list').forEach(item => {
-  item.addEventListener('click', (e)=>{
-    let x = document.getElementById(e.target.id);
-    x.contentEditable = true
-    x.addEventListener('blur', (e)=>{
-      let val = e.target.innerHTML
-      updateTask(val,e.target.id)
-      x.contentEditable = false
-    })
-  })
-})
-// let listItem = document.querySelectorAll(".todo-list-item");
-// let p = document.querySelector('.list-text')
-// listItem.forEach(item => {
-//   item.addEventListener("click", (e) => {
-//     item.style.background = "#f3f3f3";
-    
-//     console.log(p.id);
-//   });
-// })
+document.querySelectorAll('.list').forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const list = document.getElementById(e.target.id);
+    list.contentEditable = true;
+    list.style.backgroundColor = '#f3f3f3';
+    list.addEventListener('blur', (e) => {
+      const val = e.target.innerText;
+      updateTask(val, e.target.id);
+      list.contentEditable = false;
+      list.style.backgroundColor = 'white';
+    });
+  });
+});
